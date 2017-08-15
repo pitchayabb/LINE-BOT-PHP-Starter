20d6ec488e0a384e10ff4754454cccf991c16710
pitchayabb <pitchaya.supat@gmail.com>
2017-08-15 20:57:59 +0700

    ss

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'bb=='.$events['source']['roomId']
+				'text' => $event['source']['userId'].' msg '.$event['message']['id']
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
