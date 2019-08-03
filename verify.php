bc3d9f86dfc462505e0ac84ad7398ff1535d5ccf
pitchaya <pitchaya.supat@gmail.com>
2019-08-03 16:29:39 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,11 @@
 <?php
 $access_token = 'BGEDnXo2/byCnxhloFeXxhj1YZtKq43WdyFnaIgC9rLXr4aX7QxTQHnrfRDChhQzQruUv0YXHa3eHQce5Dro1b6DsDeOzI5jN5+lEaAjeXF3kDVwr/PVbhDeqO+DkmJ7BDO6zGA+3pE31GHOpE9cjgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+// $url = 'https://api.line.me/v1/oauth/verify';
+
+$url = 'https://api.line.me/oauth2/v2.1/verify';
+
+// GET https://api.line.me/oauth2/v2.1/verify
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
